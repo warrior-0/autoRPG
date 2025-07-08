05d67042668af8eea1724cf54bb9a985813de3c7
warrior-0 <bodla0330@gmail.com>
2025-07-08 10:54:43 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyAsHNjSQRToLOuFX47ZU-IfJ31xbfXmhYE",
+  authDomain: "autorpg-acb1d.firebaseapp.com",
+  projectId: "autorpg-acb1d",
+  storageBucket: "autorpg-acb1d.firebasestorage.app",
+  messagingSenderId: "363384656355",
+  appId: "1:363384656355:web:ecbbf40512f60d834b90d6",
+  measurementId: "G-1JF110QT4N"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
