5eb167a37a832db32ff329a2a6b8f4d9b73bef28
warrior-0 <bodla0330@gmail.com>
2025-07-10 20:42:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
