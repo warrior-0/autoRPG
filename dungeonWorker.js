ba47e1951d5500f672507917a44cbc988f296dd0
warrior-0 <bodla0330@gmail.com>
2025-07-18 01:24:08 +0900

    Update dungeonWorker.js

--- dungeonWorker.js
+++ dungeonWorker.js
@@ -9,31 +9,22 @@ function applyRandomVariance(value) {
   return Math.floor(value * (1 + variance));
 }
 
-function debugLog(msg) {
-  postMessage({ type: 'debug', message: msg });
-}
-
 onmessage = function (e) {
-  debugLog(`onmessage received: command=${e.data.command}`);
 
   if (e.data.command === 'start') {
     userData = e.data.userData;
-    debugLog(`start: userData received, currentStage=${userData.currentStage}`);
 
     userData.maxHp = userData.maxHp ?? 100;
     userData.hp = userData.hp ?? userData.maxHp;
 
     currentStage = Number(userData.currentStage) || 1;
-    debugLog(`currentStage set to ${currentStage}`);
 
     setupMonster();
     startCombat();
   } else if (e.data.command === 'stop') {
     clearInterval(interval);
-    debugLog("stop command received: interval cleared");
   } else if (e.data.command === 'updateUserData') {
     userData = e.data.userData;
-    debugLog("updateUserData command received");
 
     userData.maxHp = userData.maxHp ?? 100;
     userData.hp = userData.hp ?? userData.maxHp;
@@ -41,7 +32,6 @@ onmessage = function (e) {
 };
 
 function setupMonster() {
-  debugLog("setupMonster called");
   monsterHp = Math.floor((currentStage / 7 + 1) * currentStage * 2);
   monsterCrit = currentStage;
   monsterHit = currentStage * 7;
@@ -52,13 +42,10 @@ function setupMonster() {
   monsterEvasion = currentStage - 1;
   evasionRateMonster = monsterEvasion / (monsterEvasion + userData.dex * 5 + userData.str * 5 + 1);
 
-  debugLog(`Monster stats: HP=${monsterHp}, Crit=${monsterCrit}, Atk=${monsterAtk}`);
 }
 
 function startCombat() {
-  debugLog("startCombat called");
   interval = setInterval(() => {
-    debugLog("Combat tick start");
     let logMessages = [];
 
     // 몬스터 → 플레이어
@@ -77,21 +64,18 @@ function startCombat() {
         dmg = Math.max(1, dmg);
         userData.hp -= dmg;
         postMessage({ type: 'debug', message: "몬스터의 치명타 공격! 플레이어가 ${dmg} 피해를 받았습니다."});
-        debugLog(`Monster crit damage applied: ${dmg}, player HP now ${userData.hp}`);
       } else {
         dmg = Math.floor(dmg * dmgReduction);
         dmg = applyRandomVariance(dmg);
         dmg = Math.max(1, dmg);
         userData.hp -= dmg;
         postMessage({ type: 'debug', message: `플레이어가 ${dmg} 피해를 받았습니다.` });
-        debugLog(`Monster normal damage applied: ${dmg}, player HP now ${userData.hp}`);
       }
     }
 
     // 플레이어 → 몬스터
     if (Math.random() < evasionRateMonster) {
       postMessage({ type: 'debug', message: "몬스터가 플레이어 공격을 회피했습니다."});
-      debugLog("Monster evaded player attack");
     } else {
       const playerAtk = 1 + ((userData.level / 2) + 1) * userData.str * 1.5;
       const critStat = userData.dex * 5;
@@ -109,7 +93,6 @@ function startCombat() {
         monsterHp -= dmg;
         monsterHp = Math.max(0, monsterHp);
         postMessage({ type: 'debug', message: `플레이어의 치명타 공격! 몬스터가 ${dmg} 피해를 받았습니다.` });
-        debugLog(`Player crit damage applied: ${dmg}, monster HP now ${monsterHp}`);
       } else {
         dmg = Math.floor(playerAtk * dmgReductionPlayer);
         dmg = applyRandomVariance(dmg);
@@ -117,11 +100,9 @@ function startCombat() {
         monsterHp -= dmg;
         monsterHp = Math.max(0, monsterHp);
         postMessage({ type: 'debug', message: `몬스터가 ${dmg} 피해를 받았습니다.` });
-        debugLog(`Player normal damage applied: ${dmg}, monster HP now ${monsterHp}`);
       }
     }
 
-    debugLog(`Monster HP after attacks: ${monsterHp}`);
     logMessages.push(`몬스터 남은 체력: ${Math.max(0, monsterHp)}`);
 
     // 전투 결과 전송
@@ -134,7 +115,6 @@ function startCombat() {
 
     // 몬스터 사망 처리
     if (monsterHp <= 0) {
-      debugLog("Monster defeated, granting rewards");
       userData.gold += reward * 20;
       userData.exp += reward * 10;
 
@@ -150,7 +130,6 @@ function startCombat() {
 
     // 사망 처리
     if (userData.hp <= 0) {
-      debugLog("Player died, stopping combat");
       clearInterval(interval);
       postMessage({
         type: 'playerDead',
