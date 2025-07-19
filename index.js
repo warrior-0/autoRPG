0c631b0ac52a0ed4d5ae95cacc646eb3608bd224
warrior-0 <bodla0330@gmail.com>
2025-07-19 19:15:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ app.use('/api', nicknameCheckRouter);
 
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  ssl : {
+    ca: process.env.DB_SSL_CA
+  },
   charset: 'euckr',
 };
 
